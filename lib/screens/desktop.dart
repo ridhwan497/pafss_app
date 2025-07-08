9c72d4a25cbcf43b9e98d55b725bf406d9f1a6a5
ridhwan497 <ridubangire@gmail.com>
2025-07-08 12:52:52 +0300

    Just a proof of concept... These are now the different layouts

---
+++ lib/screens/desktop.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DesktopScreen extends StatelessWidget {
+  const DesktopScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Desktop Screen"),
+      ),
+    );
+  }
+  
+}
---
+++ lib/screens/tablet.dart
@@ -0,0 +1,13 @@
+// lib/screens/mobile.dart
+import 'package:flutter/material.dart';
+
+class TabletScreen extends StatelessWidget {
+  const TabletScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("📱 Tablet Screen")),
+    );
+  }
+}
